18826dafc31d54aead46476c6901410d804a1342
Tuko <tuko.ustx@gmail.com>
2025-09-26 19:55:13 -0500

    Read service

--- qtcon-ua-read/main.cpp
+++ qtcon-ua-read/main.cpp
@@ -1,19 +1,55 @@
 #include <QCoreApplication>
+#include <QOpcUaClient>
+#include <QOpcUaProvider>
+#include <QStringList>
+#include <QDebug>
+#include <QObject> // Needed for QObject::connect
+
+// Define the server endpoint URL
+const QString OpcUaEndpoint = "opc.tcp://m3:48400/UA/ComServerWrapper";
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    // Set up code that uses the Qt event loop here.
-    // Call a.quit() or a.exit() to quit the application.
-    // A not very useful example would be including
-    // #include <QTimer>
-    // near the top of the file and calling
-    // QTimer::singleShot(5000, &a, &QCoreApplication::quit);
-    // which quits the application after 5 seconds.
+    QOpcUaProvider provider;
+    if (provider.availableBackends().isEmpty())
+        return 1;
+    QOpcUaClient *client = provider.createClient(provider.availableBackends()[0]);
+    if (!client)
+        return 2;
+    // Connect to the stateChanged signal. Compatible slots of QObjects can be used instead of a lambda.
+    QObject::connect(client, &QOpcUaClient::stateChanged, [client](QOpcUaClient::ClientState state) {
+        qDebug() << "Client state changed:" << state;
+        if (state == QOpcUaClient::ClientState::Connected) {
+            QOpcUaNode *node = client->node("ns=2;s=0:TEST1/SGGN1/OUT.CV");
+            if (node) {
+                qDebug() << "A node object has been created";
 
-    // If you do not need a running Qt event loop, remove the call
-    // to a.exec() or use the Non-Qt Plain C++ Application template.
+                QObject::connect(node, &QOpcUaNode::attributeRead,
+                                 [node, client](QOpcUa::NodeAttributes attr) {
+                                     if (attr == QOpcUa::NodeAttribute::Value) {
+                                         qDebug() << "Value: " << node->attribute(QOpcUa::NodeAttribute::Value);
+
+                                         client->deleteLater();
+                                         QCoreApplication::quit();
+                                     }
+                                 });
+
+                node->readValueAttribute();
+            }
+
+        }
+    });
+
+    QObject::connect(client, &QOpcUaClient::endpointsRequestFinished,
+                     [client](QList<QOpcUaEndpointDescription> endpoints) {
+                         qDebug() << "Endpoints returned:" << endpoints.count();
+                         if (endpoints.size())
+                             client->connectToEndpoint(endpoints.first()); // Connect to the first endpoint in the list
+                     });
+
+    client->requestEndpoints(QUrl("opc.tcp://m3:48400/UA/ComServerWrapper")); // Request a list of endpoints from the server
 
     return a.exec();
 }
